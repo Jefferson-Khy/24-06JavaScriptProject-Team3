0b5d13ed54f2a4df28850fb06e5ad7ef345f1f9a
jefferson khy <khyjefferson@gmail.com>
2024-06-13 00:49:27 -0500

    added about page n dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import Login from '../src/components/Login';
 import Signup from '../src/components/Signup';
 import SubTiers from '../src/components/SubTiers';
 import NewPage from '../src/components/NewPage';
+import UserDashboard from './components/UserDashboard';
+import About from './components/About';
 
 export default function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about'element={<About />}/>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/subtiers' element={<SubTiers />} />
         <Route path='/newpage' element={<NewPage />} />
+        <Route path='/userdashboard' element={<UserDashboard/>}/>
       </Routes>
     </div>
   );
